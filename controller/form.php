4ab8e52a288b5006e9dce4ad4831f4b4bd7b9770
MNGOM <mngom@24eme.fr>
2020-06-03 17:10:22 +0200

    Ajout d'essai à modifier

--- controller/form.php
+++ controller/form.php
@@ -1,16 +1,24 @@
 <?php
 
-  $file = fopen('formulaire.csv', 'w');
+// Pour que ce soit télécharger
+header('Content-type: text/csv');
+header('Content-Disposition: attachment; filename="demo.csv"');
 
-  foreach ($variable as $key => $value) {
-    fputcsv($file, array($Q1,$Q2,$Q3,$Q4,$Q5,$Q6,$Q7,$Q8,$Q9,$Q10,$Q11,));
-    // save each row of the data
-    foreach ($data as $row)
-    {
-    fputcsv($file, $row);
-    }
-     
+// do not cache the file
+header('Pragma: no-cache');
+header('Expires: 0');
+
+if(($handler = fopen('formulaire.csv', 'w') !== FALSE){
+
+  $increment= 1;
+  $requeteQ = msqli_query('SELECT * FROM QUESTIONS WHERE idform ='..'') or die("Erreur requête: " . mysqli_error());
+  $requeteR = msqli_query('SELECT * FROM REPONSES WHERE idform='.$fef.'') or die("Erreur requête: " . mysqli_error());
+
+  foreach ($requeteQ as $data) {
+    fputcsv($file, array($increment,$Q3,$Q4,$Q5,$Q6,$Q7,$Q8,$Q9,$Q10,$Q11,));
+
   }
+}
 
 
 
---
+++ model/createUser.php
@@ -0,0 +1,11 @@
+<?php
+  require(database.php);
+
+  $username = $_POST["username"];
+  $email = $POST["email"];
+  $pwd = $_POST[];
+
+  mysqliquery();
+
+
+ ?>
