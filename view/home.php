20efc22f65eaffd09ab469a70953a8cf8f43ff54
MNGOM <mngom@24eme.fr>
2020-06-04 17:33:56 +0200

    Ajout MAJ

--- view/home.php
+++ view/home.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -13,41 +14,32 @@
   <div class="header"><h1>TreeForm<h1></div>
 <div class="container">
     <div class="create_container">
-      <form id="form" class="form" method="POST" action="form.php">
+      <form class="form" method="POST" action="form.php">
+        <div id="global_container">
           <div class="box_container">
           <label>Question : </label>
           <input class="question" name="question" type="text" placeholder="Veuillez saisir la question"  required><br>
                   <label>Réponse : </label>
                   <div id='response_container'>
-                  <input class="response" name="reponse" type="text" placeholder="Veuillez saisir la(les) réponse(s) possible(s)"  ><br>
-                </div>
-                  <div class="more more_response"><a href="" onclick="addResponse()"><img src="img/add.png"/><span>Ajouter une reponse</span></a></div>
-
+                    <input class="response" name="reponse" type="text" placeholder="Veuillez saisir la(les) réponse(s) possible(s)"  ><br>
+                    <div class="more more_response"><a onclick="addResponse()"><img src="img/add.png"/><span>Ajouter une reponse</span></a></div>
+                  </div>
           <hr>
           </div>
-          <div class="more more_question"><a href="" onclick="addQuestion()"><img src="img/add.png"/><span>Ajouter une question</span></a></div>
-
-         <!-- <div class="form-group two">
-            <input id="quizz2" name="question" type="text" placeholder="Votre question ici" class="form-control input-md" required=""><br>
-            <input id="resp21" class="response" name="reponse" type="text" placeholder="Votre reponse ici" class="form-control input-md" required=""><br>
-            <input id="resp22" class="response" name="reponse" type="text" placeholder="Votre reponse ici" class="form-control input-md" required=""><br>
-            <input id="resp23" class="response" name="reponse" type="text" placeholder="Votre reponse ici" class="form-control input-md" required=""><br>
 
         </div>
-        <hr>
-        <div class="form-group three">
-           <input id="quizz3" name="question" type="text" placeholder="Votre question ici" class="form-control input-md" required=""><br>
-           <input id="resp31" class="response" name="reponse" type="text" placeholder="Votre reponse ici" class="form-control input-md" required=""><br>
-           <input id="resp32" class="response" name="reponse" type="text" placeholder="Votre reponse ici" class="form-control input-md" required=""><br>
-           <input id="resp34" class="response" name="reponse" type="text" placeholder="Votre reponse ici" class="form-control input-md" required=""><br>
-
-       </div> -->
 
        <button type="submit" class="btn" name="btn_publish" value="true">Publier</button>
      </form>
+     <div>
+       <div class="more more_question"><a href="" onclick="addQuestion()"><img src="img/add.png"/><span>Ajouter une question</span></a></div>
+
+     </div>
     </div>
 </div>
 
 
 </body>
 </html>
+<?php
+ ?>
--- view/js/script.js
+++ view/js/script.js
@@ -1,47 +1,4 @@
-//SE RAPPELER CHECK LES BTN ET NAVIGO AUBER
-
-function addQuestion(){
-  event.preventDefault();
-  // crée un nouvel élément box_container
-    var box_container = document.createElement("div");
-    var labelQ = document.createElement("label");
-    var question = document.createElement("input");
-
-
-    var labelR = document.createElement("input");
-    var textQ = document.createElement("");
-
-
-    var response = document.createElement("input");
-  // Attribution de class
-    box_container.className = 'box_container';
-  //Initialisation des input // QUESTION:  et reponse
-    question.className = "question";
-    question.className = "question";
-
-  // Optimisation potentielle : déclarer en dehors de la fonction pour gagner de la mémoire sur la pile ou le tas ?
-    response.className = "response";
-    response.type = "text";
-    response.placeholder = "Veuillez saisir la(les) réponse(s) possible(s)";
-    response.name = "response";
-
-    var formulaire = document.getElementById("form");
-
-
-    box_container.appendChild("")
-    box_container.appendChild()
-    formulaire.appendChild(box_container);
-
-
-
-
-
-  // Retrouver le formulaire qui va le contenir
-    var formulaire = document.getElementByClassName('form');
-  // ajoute au formulaire, la div crée
-    formulaire.appendChild(div);
-}
-
+//SE RAPPELER CHECK LES BTN ET NAVIGO AUBER AJOUTER PASSING
 function addResponse(){
   //Un formulaire se recharge automatique dès qu'on ajout un input donc on prevent("empêche") l'évenement..
     event.preventDefault();
@@ -57,6 +14,73 @@ function addResponse(){
   // ajoute au formulaire, la div crée
     container.appendChild(input);
 
+};
+
+function addQuestion(e){
+  event.preventDefault();
+
+  // Retrouver le formulaire qui va le contenir
+    var global_container = document.getElementById('global_container');
+  // crée un nouvel élément box_container
+    var box_container = document.createElement("div");
+
+    //Création question et réponse
+    var labelQ = document.createElement("label");
+    var textQ = document.createTextNode("Question");
+    labelQ.appendChild(textQ);
+    var question = document.createElement("input");
 
 
+    var labelR = document.createElement("label");
+    var textR = document.createTextNode("Réponse");
+    labelR.appendChild(textR);
+    var divR = document.createElement("div");
+    var response = document.createElement("input");
+    divR.appendChild(response);
+
+    //Ajout du btn Ajouter une reponse
+    var divMore = document.createElement("div");
+    divMore.className = "more more_response";
+
+    var a = document.createElement("a");
+    a.setAttribute("onclick","addResponse()");
+    var img = document.createElement("img");
+    img.src = "img/add.png"
+    var span = document.createElement("span");
+    var textAR= document.createTextNode("Ajouter une réponse");
+
+    span.appendChild(textAR);
+    a.appendChild(img);
+    a.appendChild(span);
+    divMore.appendChild(a);
+
+
+
+    var trait = document.createElement("hr");
+
+  // Attribution de class
+    box_container.className = 'box_container';
+  //Initialisation des input // QUESTION:  et reponse
+    question.className = "question";
+    question.type = "text";
+    question.placeholder = "Veuillez saisir la question";
+    question.name = "question";
+
+  // Optimisation potentielle : déclarer en dehors de la fonction pour gagner de la mémoire sur la pile ou le tas ?
+    response.className = "response";
+    response.type = "text";
+    response.placeholder = "Veuillez saisir la(les) réponse(s) possible(s)";
+    response.name = "response";
+
+    var formulaire = document.getElementById("form");
+
+    box_container.appendChild(labelQ);
+    box_container.appendChild(question);
+    box_container.appendChild(labelR);
+    box_container.appendChild(divR);
+    box_container.appendChild(divMore);
+    box_container.appendChild(trait);
+
+    //Ajouter au formulaire
+    global_container.appendChild(box_container);
 }
