d61ab78e21dd6442c6fdb5ab2d65b69f345ecd4e
Honorine Marie Christy <honorine@24eme.fr>
2020-06-04 15:39:26 +0200

    index.php dehors

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link type="text/css" rel="stylesheet" href="css/main.css" />
+  <link rel="shortcut icon" href="img/favicon.png" />
+  <script src="js/script.js"></script>
+    <title>TreeForm</title>
+</head>
+
+<body>
+  <div class="header"><h1>TreeForm<h1></div>
+<div class="container">
+  <div class="content left_content">
+    <div class="headline">
+    <p>Creer votre formulaire simplement</p>
+    <p>Creer votre sondage</p>
+    <p>Participer,échanger avec votre communauté</p>
+    </div>
+  </div>
+  <div class=" content login_content">
+  <form action="authentification.php" method="post">
+    <div class="username_container">
+      <input type="text" class="form-control" placeholder="Pseudo ou Email">
+    </div>
+    <div class="password_container">
+      <input type="text" placeholder="Mot de passe">
+    </div>
+    <div class="btn_container">
+          <button type="submit" class="btn btn_login" name="btnlogin">Se connecter</button>
+          <button type="submit" class="btn btn_signup" name="btnsignup">S'inscrire</button>
+    </div>
+  </form>
+  </div>
+
+</div>
+
+</body>
+</html>
